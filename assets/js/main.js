f21a2776d21e42460b0ab0f73e163ed61a57488d
raveendrapujari11 <raveendrapujari11@gmail.com>
2026-02-27 09:08:29 -0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,73 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav-toggle','nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/*SCROLL HOME*/
+sr.reveal('.home__title',{}); 
+sr.reveal('.button',{delay: 200}); 
+sr.reveal('.home__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img',{}); 
+sr.reveal('.about__subtitle',{delay: 400}); 
+sr.reveal('.about__text',{delay: 400}); 
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle',{}); 
+sr.reveal('.skills__text',{}); 
+sr.reveal('.skills__data',{interval: 200}); 
+sr.reveal('.skills__img',{delay: 600});
+
+/*SCROLL WORK*/
+sr.reveal('.work__img',{interval: 200}); 
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__input',{interval: 200}); 
+
+const carousel = document.getElementById('custom-carousel');
+let scrollAmount = 0;
+const scrollStep = 266; // 250px width + 2 * 8px margin
+
+document.getElementById('scroll-left').onclick = () => {
+  scrollAmount = Math.max(0, scrollAmount - scrollStep);
+  carousel.style.transform = `translateX(-${scrollAmount}px)`;
+};
+
+document.getElementById('scroll-right').onclick = () => {
+  const maxScroll = carousel.scrollWidth - carousel.parentElement.clientWidth;
+  scrollAmount = Math.min(maxScroll, scrollAmount + scrollStep);
+  carousel.style.transform = `translateX(-${scrollAmount}px)`;
+};
